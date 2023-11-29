bb42ddaf6865f73f584fa17301201b0b9b75ce4b
alexgab <alexgabriel3357@gmail.com>
2023-11-29 14:51:35 -0300

    Implementação do padrão de projeto

---
+++ Code/chain-of-responsibility/ihandler/Request.java
@@ -0,0 +1,13 @@
+package ihandler;
+
+public class Request {
+    private String reason;
+
+    public Request(String reason) {
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+}
